89e0f7a166e081751cf340e45185fbff33fd4a06
Manas Telavane <manastelavane999@gmail.com>
2024-03-21 13:36:16 +0530

    fix: removed clear button and added autoclear

--- src/Sketch.jsx
+++ src/Sketch.jsx
@@ -7,6 +7,19 @@ const Sketch = ({ inputValue }) => {
 
     useEffect(() => {
         // Load the model on component mount or object change
+        if (modelLoaded) {
+            setModel(null); // Unload the model
+            setModelLoaded(false); // Reset model loaded state
+
+            // Clear the canvas
+            const canvas = document.querySelector('canvas');
+            const context = canvas.getContext('2d');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            // unmount the canvas
+            canvas.remove();
+        }
+
         const loadModel = async () => {
             console.log('Loading model...');
             try {
@@ -129,29 +142,6 @@ const Sketch = ({ inputValue }) => {
     return (
         <>
             <div id="sketch-container" className="h-full border-4 border-black" />
-            {
-                // Show loading message if model is not loaded
-                modelLoaded && (
-                    <button
-                        type="button"
-                        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                        onClick={() => {
-                            setModel(null); // Unload the model
-                            setModelLoaded(false); // Reset model loaded state
-
-                            // Clear the canvas
-                            const canvas = document.querySelector('canvas');
-                            const context = canvas.getContext('2d');
-                            context.clearRect(0, 0, canvas.width, canvas.height);
-
-                            // unmount the canvas
-                            canvas.remove();
-                        }}
-                    >
-                        Clear
-                    </button>
-                )
-            }
         </>
     );
 };
