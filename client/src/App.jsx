9f7f0734022c7b535e59b74d3463d8ffda0af877
manastelavane777 <manastelavane@ternaengg.ac.in>
2024-04-21 20:35:14 +0530

    feat:logout option

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,10 @@ function App() {
             setNavigateHome(true);
         }
     }, []);
+    const handleLogout = () => {
+        window.localStorage.removeItem('sketchApp');
+        setNavigateHome(false);
+    };
     return (
         <>
             {!navigateHome ? (
@@ -26,6 +30,12 @@ function App() {
                         <Sketch inputValue={inputValue} setLoading={setLoading} />
                     </div>
                     <div className="basis-1/2 flex flex-col justify-center px-5">
+                        <button
+                            className="absolute top-0 right-0 self-start text-sm text-white bg-gradient-to-br from-red-400 to-red-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-200 dark:focus:ring-red-800 font-medium rounded-lg px-3 py-2.5 my-2.5 mx-2.5"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </button>
                         <p
                             className="title shadow-black text-8xl text-green-200 font-bold drop-shadow-xl mb-2"
                             style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}
